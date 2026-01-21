d6101bab5c1d6aea9fda7852549b4ea62a2a4069
DareDevilLuc <kscasirayan@up.edu.ph>
2026-01-21 20:08:11 +0800

    good practice on scripts

--- scripts/tables.gd
+++ scripts/tables.gd
@@ -1,22 +1,8 @@
 extends Node2D
 
-const x = 32
-const y = 46
-const FRAMES = [
-	Rect2(0, 0, x, y),
-	Rect2(x, 0, x, y),
-	Rect2(x*2, 0, x, y),
-	Rect2(x*3, 0, x, y)
-]
 
 func _ready() -> void:
-	var instances = get_children()  # get all 9 instances
-	for i in instances.size():
-		var instance = instances[i]
-		# Loop frames if there are fewer than instances
-		var frame_index = i % FRAMES.size()  
-		instance.get_node("Sprite2D").region_rect = FRAMES[frame_index]
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+	for i in range(get_child_count()):
+		var child = get_child(i)
+		if child is DiffTables:
+			child.set_frame(i)
