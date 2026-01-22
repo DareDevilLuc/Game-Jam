abb597b1c15d88666d07f5d6c9277687b20cecfd
AJ <angelajahziel@gmail.com>
2026-01-23 00:31:06 +0800

    updated everythinggg, to add tomorrow boyfriend,  and interactions

--- level-3/scripts/door.gd
+++ level-3/scripts/door.gd
@@ -1,35 +1,40 @@
 extends StaticBody2D
 
-@onready var door: AnimatedSprite2D = $"../OpenDoor"
+@onready var trigger: Area2D = $trigger
+@onready var sprite: AnimatedSprite2D = $"Open Door"
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-var is_player_near := false
+var player_near := false
+var is_open := false
 
-func _on_body_entered(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
+func _ready():
+	trigger.body_entered.connect(_on_body_entered)
+	trigger.body_exited.connect(_on_body_exited)
+	sprite.animation_finished.connect(_on_animation_finished)
+	sprite.play("closed door")
 
-	is_player_near = true
-	play_open_sequence()
+func _on_body_entered(body):
+	if body.is_in_group("player") and not is_open:
+		player_near = true
+		open_door()
 
-func _on_body_exited(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
+func _on_body_exited(body):
+	if body.is_in_group("player") and is_open:
+		player_near = false
+		close_door()
 
-	is_player_near = false
-	play_close_sequence()
+func open_door():
+	is_open = true
+	sprite.play("door opening")
 
-func play_open_sequence() -> void:
-	door.play("door_opening")
-	await door.animation_finished
+func close_door():
+	is_open = false
+	sprite.play("door closing")
 
-	# Player might have left while opening
-	if is_player_near:
-		door.play("open")
-
-func play_close_sequence() -> void:
-	door.play("door_closing")
-	await door.animation_finished
-
-	# Player might have returned while closing
-	if not is_player_near:
-		door.play("close")
+func _on_animation_finished():
+	if sprite.animation == "door opening":
+		sprite.play("door open")
+		collision_shape_2d.disabled = true
+	elif sprite.animation == "door closing":
+		sprite.play("closed door")
+		collision_shape_2d.disabled = false
