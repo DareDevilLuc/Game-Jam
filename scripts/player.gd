b4a0809b21563b37da668d593eac82d8f40c58ed
RiceCell <russel.me789@gmail.com>
2026-01-23 02:21:02 +0800

    Designed. Still need to learn assetlib thing.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,34 @@
 class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN
-var move_speed : float = 150.0
-var direction : Vector2 = Vector2.ZERO
-var state : String = "idle"
+var direction : Vector2 = Vector2.ZERO #zero in x and y axes
 
-@onready var sprite: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	state_machine.Initialize(self)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	
-	direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * move_speed
-
-	if SetState() == true || SetDirection() == true:
-		UpdateAnimation()
-	
+func _process(delta):
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = direction.normalized() # This ensures the length is always 1
 	pass
 
-func _physics_process(_delta: float) -> void:
+
+func _physics_process( delta ) :
 	move_and_slide()
 
 func SetDirection() -> bool:
 	var new_dir : Vector2 = cardinal_direction
-
 	if direction == Vector2.ZERO:
 		return false
-	
+		
 	if direction.y == 0:
 		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
 	elif direction.x == 0:
@@ -43,20 +38,11 @@ func SetDirection() -> bool:
 		return false
 	
 	cardinal_direction = new_dir
-	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
-
+	sprite_2d.flip_h = (cardinal_direction == Vector2.RIGHT)
 	return true
 
-
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-
-func UpdateAnimation() -> void:
-	animation_player.play( state + "_" + AnimDirection() )
+func UpdateAnimation(state: String) -> void:
+	animation_player.play( state + "_" + AnimDirection())
 	pass
 
 func AnimDirection() -> String:
