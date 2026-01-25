d2fcfe093936c4fa743efc35e57d71372c5b6fd4
DareDevilLuc <kscasirayan@up.edu.ph>
2026-01-25 09:36:04 +0800

    sliding door

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,15 +1,30 @@
-extends Sprite2D
+extends Node2D
 
-@onready var area := $Area2D
-# Called when the node enters the scene tree for the first time.
+@export var slide_distance := 28
+@export var slide_speed := 200.0
+
+var start_position
+var target_position
+var sliding = false
+
 func _ready():
-	area.body_entered.connect(_on_body_entered)
-	area.body_exited.connect(_on_body_exited)
+	start_position = position
+	target_position = position + Vector2(slide_distance, 0)
+	
+	# Correct way to connect signals in Godot 4
+	$Area2D.body_entered.connect(Callable(self, "_on_body_entered"))
+	$Area2D.body_exited.connect(Callable(self, "_on_body_exited"))
 
 func _on_body_entered(body):
-	if body.is_in_group("player"):
-		visible = false
+	if body.name == "Player":
+		sliding = true
 
 func _on_body_exited(body):
-	if body.is_in_group("player"):
-		visible = true
+	if body.name == "Player":
+		sliding = false
+
+func _process(delta):
+	if sliding:
+		position = position.move_toward(target_position, slide_speed * delta)
+	else:
+		position = position.move_toward(start_position, slide_speed * delta)
