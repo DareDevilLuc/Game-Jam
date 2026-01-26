cd06044e25b0ccacdba3642d1691552356cbcf7c
binggbong <aissha.monceda0@gmail.com>
2026-01-26 22:13:16 +0800

    initial wedding layout

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,68 +1,31 @@
-class_name Player extends CharacterBody2D
+extends CharacterBody2D
 
-var cardinal_direction : Vector2 = Vector2.DOWN
-var move_speed : float = 150.0
-var direction : Vector2 = Vector2.ZERO
-var state : String = "idle"
+const SPEED = 150.0
 
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	
-	direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * move_speed
-
-	if SetState() == true || SetDirection() == true:
-		UpdateAnimation()
-	
-	pass
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
-	move_and_slide()
-
-func SetDirection() -> bool:
-	var new_dir : Vector2 = cardinal_direction
-
-	if direction == Vector2.ZERO:
-		return false
+	# Get the input direction for all 4 directions (WASD or Arrows)
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	# For sprite animation
+	if direction.x == 0 && direction.y == 0:
+		animated_sprite.play("idle")
 	
-	if new_dir == cardinal_direction:
-		return false
+	if direction.x > 0:
+		animated_sprite.play("walk_right")
+	elif direction.x < 0:
+		animated_sprite.play("walk_left")
 	
-	cardinal_direction = new_dir
-	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
-
-	return true
-
-
-func SetState() -> bool:
-	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
-	if new_state == state:
-		return false
-	state = new_state
-	return true
-
-func UpdateAnimation() -> void:
-	animation_player.play( state + "_" + AnimDirection() )
-	pass
-
-func AnimDirection() -> String:
-	if cardinal_direction == Vector2.DOWN:
-		return "down"
-	elif cardinal_direction == Vector2.UP:
-		return "up"
+	if direction.y > 0:
+		animated_sprite.play("walk_down")
+	elif direction.y < 0:
+		animated_sprite.play("walk_up")
+	
+	# Movement
+	if direction:
+		velocity = direction * SPEED
 	else:
-		return "side"
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+
+	move_and_slide()
