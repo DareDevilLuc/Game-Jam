2ecc469c6b260c0792a63c4f00f85667faf6314e
AJ <angelajahziel@gmail.com>
2026-01-27 12:14:32 +0800

    adding dialogue

--- level-3/scripts/partner.gd
+++ level-3/scripts/partner.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var player: CharacterBody2D = get_node("../Player") 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var trigger: Area2D = $trigger
+@onready var collision_shape_2d: CollisionShape2D = $trigger/CollisionShape2D
 
 func _physics_process(_delta):       
 	face_player()
@@ -16,3 +18,32 @@ func face_player():
 		sprite.play("idleBack")
 	else:
 		sprite.play("idleFront")
+
+@onready var balloon_scene = preload("res://dialogueBox/balloon.tscn")
+@onready var dialogue_res = preload("res://dialogue/partner.dialogue")
+var balloon: Node = null
+var dialogue_open := false
+
+func _on_trigger_body_entered(body):
+	if body.name != "Player":
+		return
+	if dialogue_open:
+		return
+
+	dialogue_open = true
+
+	balloon = balloon_scene.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_res, "partner_intro")
+
+
+	
+
+func _on_trigger_body_exited(body: Node2D) -> void:
+	if body.name != "Player":
+		return
+
+	if balloon:
+		balloon.queue_free()
+		balloon = null
+	dialogue_open = false
