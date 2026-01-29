800cacda0144f934bf37822bbdb5169a59981101
binggbong <aissha.monceda0@gmail.com>
2026-01-29 10:10:52 +0800

    reorganized for merge

---
+++ scripts/level-4/child.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var in_range: bool = false
+
+func _process(_delta) -> void:
+	if in_range == true and Input.is_action_just_pressed("interact"):
+		print("player interact")
+		DialogueManager.show_example_dialogue_balloon(load("res://assets/level-4/dialogues/npc.dialogue"), "child_start")
+		in_range = false;
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print("player in")
+		in_range = true
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		in_range = false
---
+++ scripts/level-4/partner.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var in_range: bool = false
+
+func _process(_delta) -> void:
+	if in_range == true and Input.is_action_just_pressed("interact"):
+			print("you are pressing e")
+			DialogueManager.show_example_dialogue_balloon(load("res://assets/level-4/dialogues/npc.dialogue"), "partner_start")
+			in_range = false;
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		in_range = true
+		print("bro is in range")
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		in_range = false
